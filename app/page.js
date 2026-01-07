cd64f77070d850ef81a68334e2c318e7c79d892c
aqtxv <j.hauleithner@gmail.com>
2026-01-07 16:19:01 +0100

    Create Home component for portfolio page

---
+++ app/page.js
@@ -0,0 +1,16 @@
+export default function Home() {
+  return (
+    <main style={{
+      minHeight: "100vh",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      fontFamily: "Arial, sans-serif"
+    }}>
+      <div>
+        <h1>Portfolio</h1>
+        <p>Coming soon.</p>
+      </div>
+    </main>
+  );
+}
